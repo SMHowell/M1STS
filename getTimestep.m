da2435f1f69fa0348a61d41d60d3d066e5a517ff
Samuel Howell <samuel.m.howell@jpl.nasa.gov>
2022-11-09 10:08:18 -0800

    Updated headers

--- getTimestep.m
+++ getTimestep.m
@@ -1,9 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Multiphase 1-dimensional Satellite Thermal Solver
 % Spherically symmetric solver for the heat diffusion equation, explicit
 % finite difference in time. Tailored for applications to planetary
-% reservoir freezing and evolution.
-% Sam Howell, Elodie Lesage
-% samuel.m.howell@jpl.nasa.gov
+% evolution with considerations for phase change, reservoir freezing, and 
+% eruption.
+% Sam Howell, Elodie Lesage, Julia Miller
 % (C)2022 California Institute of Technology. All rights reserved.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--- thermalSolver.m
+++ thermalSolver.m
@@ -1,8 +1,12 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%
-% Spherically symmetric thermal solver
-% Conservative finite differences, explicit forward in time
-% Samuel.M.Howell@jpl.nasa.gov
-%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Multiphase 1-dimensional Satellite Thermal Solver
+% Spherically symmetric solver for the heat diffusion equation, explicit
+% finite difference in time. Tailored for applications to planetary
+% evolution with considerations for phase change, reservoir freezing, and 
+% eruption.
+% Sam Howell, Elodie Lesage, Julia Miller
+% (C)2022 California Institute of Technology. All rights reserved.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function M = thermalSolver(M,IN,BOD)
 
