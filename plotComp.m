3952bfa1acfd2e5485973ec7433223a4c5fc14f4
ElodieLesage <elodie-lesage@hotmail.com>
2023-01-09 10:12:38 -0500

    minor fix

--- plotComp.m
+++ plotComp.m
@@ -10,13 +10,23 @@
 
 figure()
 
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{1,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{2,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{3,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{4,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{5,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{6,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{4,:}]);
-plot(OUT.t2(1:IN.outInd2),[OUT.comp{8,:}]);
+hold on
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{1,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{2,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{3,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{4,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{5,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{6,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{4,:}])
+plot(OUT.t2(1:IN.outInd2),[OUT.comp{8,:}])
+
+i=0
+for i in IN.nErupt
+    
+    i = i+1
+end
+
+
+hold off
 
 legend('Ca', 'Mg', 'Na', 'K', 'Cl', 'S', 'C', 'Si');
